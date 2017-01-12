babfcc583fd88975625c0f7b4c9e796e4a6b941a
Tim Cooijmans <cooijmans.tim@gmail.com>
2017-01-11 22:28:12 -0500

    magenta colors

--- graph.py
+++ graph.py
@@ -65,11 +65,20 @@ backwardnodes = backward(loss)
 
 radius = 0.25
 
-def mknode(node, **kwargs):
-  kwargs.setdefault("radius", radius)
-  return patches.Circle(node.x, **kwargs)
+class Colors(object):
+  darkblue = (0., 35/255., 61/255.)
+  blue = (51/255., 103/255., 214/255.)
+  lightblue = (66/255., 133/255., 244/255.)
+  aqua = (111/255., 201/255., 198/255.)
 
-def mkedge(node_a, node_b, **kwargs):
+def node_patch(node, **kwargs):
+  kwargs.setdefault("radius", radius)
+  return patches.Circle(node.x,
+                        facecolor=Colors.lightblue,
+                        edgecolor=Colors.blue,
+                        **kwargs)
+
+def edge_patch(node_a, node_b, **kwargs):
   kwargs.setdefault("width", 0.00625)
   kwargs.setdefault("length_includes_head", True)
   a, b = np.asarray(node_a.x), np.asarray(node_b.x)
@@ -80,7 +89,10 @@ def mkedge(node_a, node_b, **kwargs):
   a = a + radius * u
   # shorten edge by 2 * radius to go from perimeter to perimeter
   dx = dx - 2 * radius * u
-  return patches.FancyArrow(a[0], a[1], dx[0], dx[1], **kwargs)
+  return patches.FancyArrow(a[0], a[1], dx[0], dx[1],
+                            facecolor=Colors.blue,
+                            edgecolor=Colors.blue,
+                            **kwargs)
 
 for nodes in [forwardnodes, backwardnodes]:
   fix, ax = plt.subplots(1)
